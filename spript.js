d646aa54160a119d3e82617ce9439a5670f1a95b
Nightfeller <family2earls@gmail.com>
2023-08-28 21:59:47 -0500

    basic questions and answers

---
+++ spript.js
@@ -0,0 +1,127 @@
+var startButton = document.querySelector("#start");
+var asking = document.querySelector("#count");
+var questionLine = document.querySelector("#question");
+var isAnswerRight = document.querySelector("#response");
+var startingSeconds = 3;
+var incri = 1;
+var score = 0;
+var timeLeft = 30;
+var option1 = document.querySelector(".one");
+var option2 = document.querySelector(".two");
+var option3 = document.querySelector(".three");
+var option4 = document.querySelector(".four");
+var q1Array = ["", "HTML, Python, CSS", "CSS, JavaScript, HTML", "Lua, JavaScript, CSS", "Python, C#, C++"];
+var q2Array = ["", "while/loop/loop", "for/while/loop", "for/loop/loop", "for/loop/iteration"];
+var q3Array = ["", "Brendan Eich", "Elon Musk", "Mitchell Baker", "Nolan Bushnell"];
+var q4Array = ["", "data-state: hidden", "opacity: 0", "object = hidden", "None of the above"];
+var q5Array = ["", "isKeyPressed()", "buttonInput()", "checkKeyDown()", "addEventListener()"];
+/*
+var keys = isKeyPressed();
+
+function isKeyPressed() {
+    addEventListener("keydown", function(event) {
+        var keyType = event.key;
+        console.log(keyType);
+        return keyType;
+    });
+}
+*/
+
+function timerFunction(){
+    var timerInterval = setInterval(function() {
+        startingSeconds--;
+        asking.textContent = "Starting in " + startingSeconds + " seconds.";
+
+        if (startingSeconds === 0){
+            clearInterval(timerInterval);
+            startQuiz();
+        }
+    }, 1000)
+}
+
+function inertia(opt) {
+    console.log(opt);
+    if ((incri == 1 && opt == 2) || (incri == 2 && opt == 3) || (incri == 3 && opt == 1) || (incri == 4 && opt == 4) || (incri == 5 && opt == 4)){
+        score++;
+        isAnswerRight.textContent = "Correct";
+    } else {
+        isAnswerRight.textContent = "Incorrect";
+    }
+    incri++;
+    startQuiz();
+}
+
+// Questions:
+// 1: What are the primary languages in coding?
+// 1st Options: (HTML, Python, CSS), (CSS, JavaScript, HTML), (Lua, JavaScript, CSS), (Python, C#, C++). Answer: 2
+
+// 2: A ___ ____ iterates lines of code multipute times until the ____ stops.
+// 2nd Options: (while, loop, loop), (for, while, loop), (for, loop, loop), (for, loop, iteration). Answer: 3
+
+// 3: Who first made JavaScript?
+// 3rd Options: (Brendan Eich), (Elon Musk), (Mitchell Baker), (Nolan Bushnell). Answer: 1
+
+// 4: How can an object be turned insivible?
+// 4th Options: (data-state: hidden), (set-visiblity: 0), (object = hidden), (None of the above). Answer: 4
+
+// 5: The ________________ checks for button inputs to activate lines of code.
+// 5th Options: (isKeyPressed), (buttonInput), (checkKeyDown), (addEventListener). Answer: 4
+
+function startQuiz() {
+
+    option1.addEventListener("click", function(event) {
+        inertia(1);
+    });
+    option2.addEventListener("click", function(event) {
+        inertia(2);
+    });
+    option3.addEventListener("click", function(event) {
+        inertia(3);
+    });
+    option4.addEventListener("click", function(event) {
+        inertia(4);
+    });
+    
+    if (incri == 1) {
+        asking.textContent = "Question 1:";
+        questionLine.textContent = "What are the primary languages in coding?";
+        option1.textContent = q1Array[1];
+        option2.textContent = q1Array[2];
+        option3.textContent = q1Array[3];
+        option4.textContent = q1Array[4];
+    } else if (incri == 2) {
+        asking.textContent = "Question 2:";
+        questionLine.textContent = "A ___ ____ iterates lines of code multiple times until the ____ stops."
+        option1.textContent = q2Array[1];
+        option2.textContent = q2Array[2];
+        option3.textContent = q2Array[3];
+        option4.textContent = q2Array[4];
+    } else if (incri == 3) {
+        asking.textContent = "Question 3:";
+        questionLine.textContent = "Who first made JavaScript?"
+        option1.textContent = q3Array[1];
+        option2.textContent = q3Array[2];
+        option3.textContent = q3Array[3];
+        option4.textContent = q3Array[4];
+    } else if (incri == 4) {
+        asking.textContent = "Question 4:";
+        questionLine.textContent = "How can an object be turned insivible?"
+        option1.textContent = q4Array[1];
+        option2.textContent = q4Array[2];
+        option3.textContent = q4Array[3];
+        option4.textContent = q4Array[4];
+    } else if (incri == 5) {
+        asking.textContent = "Question 5:";
+        questionLine.textContent = "The ________________ checks for button inputs to activate lines of code."
+        option1.textContent = q5Array[1];
+        option2.textContent = q5Array[2];
+        option3.textContent = q5Array[3];
+        option4.textContent = q5Array[4];
+    }
+}
+
+startButton.addEventListener("click", function(event) {
+    startButton.setAttribute("style", "display:none;");
+    timerFunction();
+    asking.textContent = "Starting in " + startingSeconds + " seconds."
+});
